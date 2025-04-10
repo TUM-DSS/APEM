811575019beea3dc9aa6ba89c5ef472f3d7efaf3
mathewsl <laura.mathews@tum.de>
2025-04-10 13:49:45 +0200

    update test files to work with new hybrid layout by modifying sys.path

--- tests/test_zonal_ntc.py
+++ tests/test_zonal_ntc.py
@@ -1,5 +1,7 @@
 import unittest
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from apem.execution_chain import solve_scenario, Datasets, PowerFlowModels, PricingAlgorithms
 
